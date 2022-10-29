bd0f8876daaa57acfdc621709c1f2ccba7903dac
saman sayyar <samansayyar.dev@gmail.com>
2022-10-29 10:04:27 +0330

    first2

--- components/layouts/Header.js
+++ components/layouts/Header.js
@@ -5,6 +5,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 import CardExplore from "../cards/CardExplore";
 import CardVideo from "../cards/CardVideo";
+import { data } from "autoprefixer";
 
 
 export default function Header() {
@@ -74,8 +75,10 @@ export default function Header() {
 
         <section className="mt-10 w-full relative">
           <div dir="ltr" className="grid place-content-center place-items-center grid-cols-12 gap-2 gap-y-4 w-full mt-6">
-            {Items.sort((a, b) => a > b ? -1 : (a > b ? 1 : 0)).map(data => (
+            {Items.sort((a, b) => a > b ? -1 : (a > b ? 1 : 0)).map((data, index) => (
+              <div key={index}>
                 <CardVideo />
+              </div>
             ))}
           </div>
         </section>
